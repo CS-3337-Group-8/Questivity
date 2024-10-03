72bc6d2d8d529a55089e6bdd169d6ae52e7f341f
Juan La serna <juanlaserna@Juans-MacBook-Pro.local>
2024-10-02 18:26:36 -0700

    your message here

--- GoDot/FrontScene.gd
+++ GoDot/FrontScene.gd
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 extends Control
 
 #variables
@@ -58,30 +57,3 @@ func _on_buttonLogin_pressed():
 #Create New Account Button
 func _on_buttonCreateAcc_pressed():
 	get_tree().change_scene("res://NewAccountPage.tscn") #moves to New Account Page
-=======
-extends Control
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-#Login button
-func _on_buttonLogin_pressed():
-	get_tree().change_scene("res://MainPage.tscn")   #moves to Main Page
-
-#Create New Account Button
-func _on_buttonCreateAcc_pressed():
-	get_tree().change_scene("res://NewAccountPage.tscn") #moves to New Account Page
->>>>>>> main
