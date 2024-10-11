d0590f0e05ff36c6b742dfadc8e2a7ec6d8b3638
beijing-software <115838717+beijing-software@users.noreply.github.com>
2024-10-10 20:31:00 -0700

    update

--- docker/server-api/src/app.py
+++ docker/server-api/src/app.py
@@ -1,40 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 import os
 
 app = Flask(__name__)
 
+external_api_url = 'http://db-api:3000'
+headers = {'x-api-key': os.getenv('DB_API_KEY')}
+
+def fetch(url, method='GET', headers=None, json_data=None):
+    response = requests.request(method, url, headers=headers, json=json_data)
+    response.raise_for_status()
+
+    if (response.headers.get('Content-Type') == 'application/json'):
+        return response.json()
+    
+    return {
+        "message" : response.text,
+        "status_code" : response.status_code,
+    }
 
 @app.route('/')
 def home():
     return "Hello, World!"
 
-
 @app.route('/fetch-data', methods=['GET'])
-def fetch_data():
-    try:
-        external_api_url = 'http://db-api:3000/'
-        api_key = os.getenv('API_KEY')
-
-        headers = {
-            'x-api-key': api_key
-        }
-
-        response = requests.get(external_api_url, headers=headers)
-
-
-        app.logger.info(f"Response status code: {response.status_code}")
-        app.logger.info(f"Response text: {response.text}")
-
-
-        if response.status_code == 200:
-            return response.text, 200 
-        else:
-            app.logger.error(f"Failed to fetch data: {response.status_code} - {response.text}")
-            return jsonify({"error": "Failed to fetch data from external API"}), response.status_code
-    except requests.exceptions.RequestException as e:
-        app.logger.error(f"Request exception: {str(e)}")
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+def fetch_data():    
+    return fetch(external_api_url, method='GET', headers=headers)
