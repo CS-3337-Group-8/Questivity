7d15fd1cc904aaccb7ca3e269bc43edd79384b8b
beijing-software <115838717+beijing-software@users.noreply.github.com>
2024-10-12 10:38:29 -0700

    update

--- docker/questivity-server/src/app.py
+++ docker/questivity-server/src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 import os
 
 app = Flask(__name__)
+CORS(app)
 
-external_api_url = 'http://db-api:3000'
+external_api_url = 'http://questivitydb-api:3000'
 headers = {'x-api-key': os.getenv('DB_API_KEY')}
 
 def fetch(url, method='GET', headers=None, json_data=None):
@@ -21,8 +23,11 @@ def fetch(url, method='GET', headers=None, json_data=None):
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Questivity Server Version 1.0.0"
 
 @app.route('/fetch-data', methods=['GET'])
 def fetch_data():    
     return fetch(external_api_url, method='GET', headers=headers)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
